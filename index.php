2d42b3625e80166fa82f3564b8c505ea63dc3f84
bryan-qiu <bryan_qiu@hotmail.com>
2015-06-01 22:32:55 -0400

    added bootstrap

--- index.php
+++ index.php
@@ -2,23 +2,58 @@
 <?php 
 	include("./php/functions.php");
 	//findFreeGame('Padres');
+	//updateDatabase();
 ?>
-<html>
-<head>
-	<title>MLB Free Game</title>
-	<link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
-  	<script src="//code.jquery.com/jquery-1.11.1.js"></script>
-  	<script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
-  	<script type="text/javascript" src="./js/functions.js"></script>
-	<meta name="description" content="Search MLB.TV's free game of the game"/>
-	<meta name="author" content="Bryan Qiu"/>
-	<meta charset="UTF-8">
-</head>
-<body>
-	<div class="ui-widget">
-	  <label for="tags">Tags: </label>
-	  <input id="tags" name="tags">
-	</div>
-	<div id="list"></div>
-</body>
+<html lang="en">
+	<head>
+    	<meta charset="utf-8">
+	    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+	    <title>MLB Free Game</title>
+
+	    <link rel="stylesheet" href="bootstrap-3.3.4-dist/css/bootstrap.min.css">
+	    <link rel="stylesheet" type="text/css" href="./css/style.css"/>
+		<link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+
+	    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+	  	<script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+	    <script src="bootstrap-3.3.4-dist/js/bootstrap.min.js"></script>
+	  	<script type="text/javascript" src="./js/functions.js"></script>
+		<meta name="description" content="Search MLB.TV's free game of the game"/>
+		<meta name="author" content="Bryan Qiu"/>
+  	</head>
+  	<body>
+  		
+  		<div class="content">
+  			<div class="container">
+			    <!--<div class="row">
+  					<img src="./img/banner.png" class="img-responsive center-block" />
+  				</div>-->
+  				<br>
+  				<br>
+  				<div class="row row-centered">
+  					<div class="col-lg-3 col-centered">
+					    <div class="ui-widget">
+							<label for="tags">Teams: </label>
+							<input id="tags" name="tags" style="width:100%; text-align:center">
+					    </div>
+					    <div id = "list"></div>
+					</div>
+			    </div>
+			</div>
+
+  		</div>
+
+  		<!--<div class="ui-widget">
+  			<label for="tags">Teams: </label>
+		  	<input id="tags" name="tags">
+		</div>
+		<div id="list"></div>-->
+
+		<div class="footer">
+			<div class="container-fluid">
+			</div>
+		</div>
+  	</body>
 </html>
